e53738e6bd9e75aeaa7114629ce75aeafb5aa3e3
Kevin De Paula <kde-paul@c1r5s11.42lisboa.com>
2026-02-22 14:51:43 +0000

    ex1 of module 01 was modificated

--- module_01/ex1/ft_garden_data.py
+++ module_01/ex1/ft_garden_data.py
@@ -6,9 +6,9 @@ class Plant:
 
 
 if __name__ == "__main__":
-    rose = Plant("Rose", 5, 3)
-    sunflower = Plant("Sunflower", 15, 35)
-    cactus = Plant("Cactus", 51, 53)
+    rose = Plant("Rose", 25, 30)
+    sunflower = Plant("Sunflower", 80, 45)
+    cactus = Plant("Cactus", 15, 120)
     print(f"""=== Garden Plant Registry ===
 {rose.name}: {rose.height}cm, {rose.age} days old
 {sunflower.name}: {sunflower.height}cm, {sunflower.age} days old
